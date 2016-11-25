e06846a9a32b3cf90e30a1d1de5b8406e63e9d8b
Tuwuh Sarwoprasojo <tuwuhs@gmail.com>
2016-11-26 00:36:23 +0700

    Convert from OpenCV format to ARToolkit format

--- yml2dat/yml2dat.cpp
+++ yml2dat/yml2dat.cpp
@@ -1,12 +1,17 @@
 /* yml2dat.cpp
+ * Based on artoolkit5 utility calib_camera.cpp
  * (c) 2016 Tuwuh Sarwoprasojo
  */
 
 #include <iostream>
 #include <opencv2/opencv.hpp>
+#include <AR/ar.h>
 
 using namespace cv;
 using namespace std;
+
+static void convParam(float intr[3][4], float dist[4], int xsize, int ysize, ARParam *param);
+static ARdouble getSizeFactor(ARdouble dist_factor[], int xsize, int ysize, int dist_function_version);
 
 int main(int argc, char* argv[])
 {
@@ -19,13 +24,194 @@ int main(int argc, char* argv[])
 	cout << "Reading file " << filename << "..." << endl;
 	FileStorage fs(filename, FileStorage::READ);
 
-	Mat cameraMatrix, distCoeffs;
-	fs["camera_matrix"] >> cameraMatrix;
-	fs["distortion_coefficients"] >> distCoeffs;
+	Mat camera_matrix, dist_coeffs;
+	int xsize, ysize;
+	fs["camera_matrix"] >> camera_matrix;
+	fs["distortion_coefficients"] >> dist_coeffs;
+	fs["image_width"] >> xsize;
+	fs["image_height"] >> ysize;
 
-	cout << cameraMatrix << endl;
-	cout << distCoeffs << endl;
+	cout << "camera_matrix: " << camera_matrix << endl;
+	cout << "dist_coeffs: " << dist_coeffs << endl;
+	cout << "xsize: " << xsize << ", ysize:" << ysize << endl;
+
+	float intr[3][4];
+	float dist[4];
+	for (int j = 0; j < 3; j++) {
+		for (int i = 0; i < 3; i++) {
+			intr[j][i] = static_cast<float>(camera_matrix.at<double>(j, i));
+		}
+		intr[j][3] = 0.0f;
+	}
+	for (int i = 0; i < 4; i++) {
+		dist[i] = static_cast<float>(dist_coeffs.at<double>(i));
+	}
+
+	ARParam param;
+	convParam(intr, dist, xsize, ysize, &param);
+	arParamDisp(&param);
 
 	return 0;
 }
 
+static void convParam(float intr[3][4], float dist[4], int xsize, int ysize, ARParam *param)
+{
+	ARdouble s;
+	int i, j;
+
+	param->dist_function_version = 4;
+	param->xsize = xsize;
+	param->ysize = ysize;
+
+	param->dist_factor[0] = (ARdouble)dist[0];     /* k1  */
+	param->dist_factor[1] = (ARdouble)dist[1];     /* k2  */
+	param->dist_factor[2] = (ARdouble)dist[2];     /* p1  */
+	param->dist_factor[3] = (ARdouble)dist[3];     /* p2  */
+	param->dist_factor[4] = (ARdouble)intr[0][0];  /* fx  */
+	param->dist_factor[5] = (ARdouble)intr[1][1];  /* fy  */
+	param->dist_factor[6] = (ARdouble)intr[0][2];  /* x0  */
+	param->dist_factor[7] = (ARdouble)intr[1][2];  /* y0  */
+	param->dist_factor[8] = (ARdouble)1.0;         /* s   */
+
+	for (j = 0; j < 3; j++) {
+		for (i = 0; i < 4; i++) {
+			param->mat[j][i] = (ARdouble)intr[j][i];
+		}
+	}
+
+	s = getSizeFactor(param->dist_factor, xsize, ysize, param->dist_function_version);
+	param->mat[0][0] /= s;
+	param->mat[0][1] /= s;
+	param->mat[1][0] /= s;
+	param->mat[1][1] /= s;
+	param->dist_factor[8] = s;
+}
+
+static ARdouble getSizeFactor(ARdouble dist_factor[], int xsize, int ysize, int dist_function_version)
+{
+	ARdouble  ox, oy, ix, iy;
+	ARdouble  olen, ilen;
+	ARdouble  sf, sf1;
+
+	sf = 100.0;
+
+	ox = 0.0;
+	oy = dist_factor[7];
+	olen = dist_factor[6];
+	arParamObserv2Ideal(dist_factor, ox, oy, &ix, &iy, dist_function_version);
+	ilen = dist_factor[6] - ix;
+	//ARLOG("Olen = %f, Ilen = %f, s = %f\n", olen, ilen, ilen / olen);
+	if (ilen > 0) {
+		sf1 = ilen / olen;
+		if (sf1 < sf) sf = sf1;
+	}
+
+	ox = xsize;
+	oy = dist_factor[7];
+	olen = xsize - dist_factor[6];
+	arParamObserv2Ideal(dist_factor, ox, oy, &ix, &iy, dist_function_version);
+	ilen = ix - dist_factor[6];
+	//ARLOG("Olen = %f, Ilen = %f, s = %f\n", olen, ilen, ilen / olen);
+	if (ilen > 0) {
+		sf1 = ilen / olen;
+		if (sf1 < sf) sf = sf1;
+	}
+
+	ox = dist_factor[6];
+	oy = 0.0;
+	olen = dist_factor[7];
+	arParamObserv2Ideal(dist_factor, ox, oy, &ix, &iy, dist_function_version);
+	ilen = dist_factor[7] - iy;
+	//ARLOG("Olen = %f, Ilen = %f, s = %f\n", olen, ilen, ilen / olen);
+	if (ilen > 0) {
+		sf1 = ilen / olen;
+		if (sf1 < sf) sf = sf1;
+	}
+
+	ox = dist_factor[6];
+	oy = ysize;
+	olen = ysize - dist_factor[7];
+	arParamObserv2Ideal(dist_factor, ox, oy, &ix, &iy, dist_function_version);
+	ilen = iy - dist_factor[7];
+	//ARLOG("Olen = %f, Ilen = %f, s = %f\n", olen, ilen, ilen / olen);
+	if (ilen > 0) {
+		sf1 = ilen / olen;
+		if (sf1 < sf) sf = sf1;
+	}
+
+	ox = 0.0;
+	oy = 0.0;
+	arParamObserv2Ideal(dist_factor, ox, oy, &ix, &iy, dist_function_version);
+	ilen = dist_factor[6] - ix;
+	olen = dist_factor[6];
+	//ARLOG("Olen = %f, Ilen = %f, s = %f\n", olen, ilen, ilen / olen);
+	if (ilen > 0) {
+		sf1 = ilen / olen;
+		if (sf1 < sf) sf = sf1;
+	}
+	ilen = dist_factor[7] - iy;
+	olen = dist_factor[7];
+	//ARLOG("Olen = %f, Ilen = %f, s = %f\n", olen, ilen, ilen / olen);
+	if (ilen > 0) {
+		sf1 = ilen / olen;
+		if (sf1 < sf) sf = sf1;
+	}
+
+	ox = xsize;
+	oy = 0.0;
+	arParamObserv2Ideal(dist_factor, ox, oy, &ix, &iy, dist_function_version);
+	ilen = ix - dist_factor[6];
+	olen = xsize - dist_factor[6];
+	//ARLOG("Olen = %f, Ilen = %f, s = %f\n", olen, ilen, ilen / olen);
+	if (ilen > 0) {
+		sf1 = ilen / olen;
+		if (sf1 < sf) sf = sf1;
+	}
+	ilen = dist_factor[7] - iy;
+	olen = dist_factor[7];
+	//ARLOG("Olen = %f, Ilen = %f, s = %f\n", olen, ilen, ilen / olen);
+	if (ilen > 0) {
+		sf1 = ilen / olen;
+		if (sf1 < sf) sf = sf1;
+	}
+
+	ox = 0.0;
+	oy = ysize;
+	arParamObserv2Ideal(dist_factor, ox, oy, &ix, &iy, dist_function_version);
+	ilen = dist_factor[6] - ix;
+	olen = dist_factor[6];
+	//ARLOG("Olen = %f, Ilen = %f, s = %f\n", olen, ilen, ilen / olen);
+	if (ilen > 0) {
+		sf1 = ilen / olen;
+		if (sf1 < sf) sf = sf1;
+	}
+	ilen = iy - dist_factor[7];
+	olen = ysize - dist_factor[7];
+	//ARLOG("Olen = %f, Ilen = %f, s = %f\n", olen, ilen, ilen / olen);
+	if (ilen > 0) {
+		sf1 = ilen / olen;
+		if (sf1 < sf) sf = sf1;
+	}
+
+	ox = xsize;
+	oy = ysize;
+	arParamObserv2Ideal(dist_factor, ox, oy, &ix, &iy, dist_function_version);
+	ilen = ix - dist_factor[6];
+	olen = xsize - dist_factor[6];
+	//ARLOG("Olen = %f, Ilen = %f, s = %f\n", olen, ilen, ilen / olen);
+	if (ilen > 0) {
+		sf1 = ilen / olen;
+		if (sf1 < sf) sf = sf1;
+	}
+	ilen = iy - dist_factor[7];
+	olen = ysize - dist_factor[7];
+	//ARLOG("Olen = %f, Ilen = %f, s = %f\n", olen, ilen, ilen / olen);
+	if (ilen > 0) {
+		sf1 = ilen / olen;
+		if (sf1 < sf) sf = sf1;
+	}
+
+	if (sf == 100.0) sf = 1.0;
+
+	return sf;
+}
